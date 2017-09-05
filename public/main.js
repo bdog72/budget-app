2c3b914e190899794d2911c6db8da17a5679db97
Brian Bycynski <high5bri@hotmail.com>
2017-09-05 08:55:40 -0400

    finished section 6 lecture 69

--- public/main.js
+++ public/main.js
@@ -1,5 +1,38 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
-}
 
-document.addEventListener('DOMContentLoaded', main)
+// BUDGET CONTROLLER
+var budgetController = (function () {
+
+  // some code
+
+})()
+
+// UI CONTROLLER
+var UIController = (function () {
+
+  //  some code
+
+})()
+
+// GLOBAL APP CONTROLLER
+var controller = (function (budgetCtrl, UICtrl) {
+  var ctrlAddItem = function () {
+    // 1. Get field input data
+
+    // 2. add the item to the budget controller
+
+    // 3. add the item to the UI
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on the UI
+    console.log('It works')
+  }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+
+  document.addEventListener('keypress', function (event) {
+    if (event.keyCode === 13) {
+      ctrlAddItem()
+    }
+  })
+})(budgetController, UIController)
