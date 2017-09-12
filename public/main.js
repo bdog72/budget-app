cae56b8a0b579dd4b84bc1639009308336e3c3d2
Brian Bycynski <high5bri@hotmail.com>
2017-09-12 08:13:38 -0400

    made few changes

--- public/main.js
+++ public/main.js
@@ -5,6 +5,19 @@ var budgetController = (function () {
     this.id = id
     this.description = description
     this.value = value
+    this.percentage = -1
+  }
+
+  Expense.prototype.calcPercentage = function (totalIncome) {
+    if (totalIncome > 0) {
+      this.percentage = Math.round((this.value / totalIncome) * 100)
+    } else {
+      this.percentage = -1
+    }
+  }
+
+  Expense.prototype.getPercentage = function () {
+    return this.percentage
   }
 
   var Income = function (id, description, value) {
@@ -89,7 +102,16 @@ var budgetController = (function () {
     },
 
     calculatePercentages: function () {
+      data.allItems.exp.forEach(function (cur) {
+        cur.calcPercentage(data.totals.inc)
+      })
+    },
 
+    getPercentages: function () {
+      var allPerc = data.allItems.exp.map(function (cur) {
+        return cur.getPercentage()
+      })
+      return allPerc
     },
 
     getBudget: function () {
@@ -215,13 +237,12 @@ var controller = (function (budgetCtrl, UICtrl) {
   }
 
   var updatePercentages = function () {
-
     // 1. Calculate percentages
-
+    budgetCtrl.calculatePercentages()
     // 2. Read the percentages from the budget controller
-
+    var percentages = budgetCtrl.getPercentages()
     // 3. Update the UI with the new percentages
-
+    console.log(percentages)
   }
 
   var ctrlAddItem = function () {
