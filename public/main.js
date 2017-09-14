c28575af62b92e0b1f26ca6f0463b3a7a7616de7
Brian Bycynski <high5bri@hotmail.com>
2017-09-14 10:43:51 -0400

    finished budget app

--- public/main.js
+++ public/main.js
@@ -143,7 +143,8 @@ var UIController = (function () {
     expensesLabel: '.budget__expenses--value',
     percentageLabel: '.budget__expenses--percentage',
     container: '.container',
-    expensesPercLabel: '.item__percentage'
+    expensesPercLabel: '.item__percentage',
+    dateLabel: '.budget__title--month'
   }
 
   var formatNumber = function (num, type) {
@@ -161,6 +162,12 @@ var UIController = (function () {
     dec = numSplit[1]
 
     return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec
+  }
+
+  var nodeListForEach = function (list, callback) {
+    for (var i = 0; i < list.length; i++) {
+      callback(list[i], i)
+    }
   }
 
   return {
@@ -229,12 +236,6 @@ var UIController = (function () {
     displayPercentages: function (percentages) {
       var fields = document.querySelectorAll(DOMstrings.expensesPercLabel)
 
-      var nodeListForEach = function (list, callback) {
-        for (var i = 0; i < list.length; i++) {
-          callback(list[i], i)
-        }
-      }
-
       nodeListForEach(fields, function (current, index) {
         if (percentages[index] > 0) {
           current.textContent = percentages[index] + '%'
@@ -242,6 +243,31 @@ var UIController = (function () {
           current.textContent = '---'
         }
       })
+    },
+
+    displayMonth: function () {
+      var now, months, month, year
+
+      now = new Date()
+
+      months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+      month = now.getMonth()
+      year = now.getFullYear()
+      document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year
+    },
+
+    changedType: function () {
+      var fields = document.querySelectorAll(
+        DOMstrings.inputType + ',' +
+        DOMstrings.inputDescription + ',' +
+        DOMstrings.inputValue
+      )
+
+      nodeListForEach(fields, function (cur) {
+        cur.classList.toggle('red-focus')
+      })
+
+      document.querySelector(DOMstrings.inputBtn).classList.toggle('red')
     },
 
     getDOMstrings: function () {
@@ -262,6 +288,8 @@ var controller = (function (budgetCtrl, UICtrl) {
       }
     })
     document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem)
+
+    document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType)
   }
 
   var updateBudget = function () {
@@ -332,6 +360,7 @@ var controller = (function (budgetCtrl, UICtrl) {
   return {
     init: function () {
       console.log('Application has started')
+      UICtrl.displayMonth()
       UICtrl.displayBudget({
         budget: 0,
         totalInc: 0,
@@ -345,7 +374,8 @@ var controller = (function (budgetCtrl, UICtrl) {
 
 controller.init()
 
-// 
+// Finished lecture 88
+//
 // // BUDGET CONTROLLER
 // var budgetController = (function () {
 //   var Expense = function (id, description, value) {
